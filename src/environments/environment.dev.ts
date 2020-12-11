7438f3f91640d04d39522800e6889593e52055cd
kevin <kevin.vandenelshout@gmail.com>
2020-12-11 11:17:04 +0100

    set correct dev urls for aws
    Issues: #5

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://vis-dev.inbo.be',
+  apiUrl: 'https://vis2-api-dev.inbo.be',
   keycloakClientId: 'vis-dev',
   keycloakIssuer: "https://keycloak-dev.inbo.be/auth/realms/Inbo-Extranet",
   showDebugInformation: true
