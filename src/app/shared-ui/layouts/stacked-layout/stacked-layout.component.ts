a4a641b981d775d5bee6dfef90f9f19cfd8c3ad0
kevin <kevin.vandenelshout@gmail.com>
2020-12-14 12:04:36 +0100

    quick project search

--- src/app/shared-ui/layouts/stacked-layout/stacked-layout.component.ts
+++ src/app/shared-ui/layouts/stacked-layout/stacked-layout.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {NavigationLink} from "../NavigationLinks";
 import {AuthService} from "../../../core/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'stacked-layout',
@@ -11,14 +12,23 @@ export class StackedLayoutComponent implements OnInit {
   @Input() navigationLinks: NavigationLink[];
 
   public isOpen: boolean = false;
+  public search: string;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
 
   logout() {
     this.authService.logout();
+  }
+
+  searchProject(event: KeyboardEvent) {
+    if (event.code === 'Enter') {
+      this.router.navigateByUrl("/projecten?name=" + this.search)
+    }
+
   }
 
   public get fullName() {
