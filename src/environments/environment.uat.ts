7737b3910d92f20e809b0275665c2a2dc29bdd26
kevin <kevin.vandenelshout@gmail.com>
2020-12-15 11:50:25 +0100

    update uat properties
    Issues: #5

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vis-uat.inbo.be',
+  apiUrl: 'https://vis2-api-uat.inbo.be',
   keycloakClientId: 'vis-uat',
   keycloakIssuer: "https://keycloak-uat.inbo.be/auth/realms/Inbo-Extranet",
   showDebugInformation: false
