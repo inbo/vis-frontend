b2cdd6d9d94dce2434088cc5714a28a41c797c4a
kevin <kevin.vandenelshout@gmail.com>
2021-03-19 11:56:25 +0100

    temp commit

--- src/app/vis/survey-events/survey-event-measurements-create-page/survey-event-measurements-create-page.component.ts
+++ src/app/vis/survey-events/survey-event-measurements-create-page/survey-event-measurements-create-page.component.ts
@@ -3,7 +3,7 @@ import {NavigationLink} from '../../../shared-ui/layouts/NavigationLinks';
 import {GlobalConstants} from '../../../GlobalConstants';
 import {BreadcrumbLink} from '../../../shared-ui/breadcrumb/BreadcrumbLinks';
 import {ActivatedRoute} from '@angular/router';
-import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-survey-event-measurements-create-page',
@@ -36,12 +36,22 @@ export class SurveyEventMeasurementsCreatePageComponent implements OnInit, After
     }
   ];
 
-  measurements = new FormArray([]);
+  private measurementsForm: FormGroup;
+  private items: FormArray;
 
-  private hasListener = false;
+  constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) {
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.measurements.push(new FormGroup({
+
+  }
+
+  ngOnInit(): void {
+    this.measurementsForm = this.formBuilder.group({
+      items: this.formBuilder.array([ this.createItem() ])
+    });
+  }
+
+  createItem(): FormGroup {
+    return this.formBuilder.group({
       species: new FormControl('', Validators.required),
       length: new FormControl('', Validators.required),
       weight: new FormControl('', Validators.required),
@@ -49,56 +59,23 @@ export class SurveyEventMeasurementsCreatePageComponent implements OnInit, After
       gender: new FormControl('', Validators.required),
       lengthMeasurement: new FormControl('', Validators.required),
       comment: new FormControl('', Validators.required)
-    }));
-  }
-
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit() {
-    let commentFields = document.getElementsByName('comment');
-    const commentField = commentFields[commentFields.length - 1];
-
-    this.addEventListener(commentField);
-  }
-
-  ngAfterViewChecked(): void {
-    if (this.hasListener) {
-      return;
-    }
-
-    const commentFields = document.getElementsByName('comment');
-    const commentField = commentFields[commentFields.length - 1];
-
-    this.addEventListener(commentField);
-
-    const speciesFields = document.getElementsByName('species');
-    setTimeout(() => {
-      speciesFields[speciesFields.length - 1].focus();
     });
   }
 
-  private addEventListener(commentField: HTMLElement) {
-    const listener = event => {
-      if (event.key === 'Tab') {
-        event.preventDefault();
+  ngAfterViewInit() {
+  }
 
-        this.measurements.push(new FormGroup({
-          species: new FormControl('', Validators.required),
-          length: new FormControl('', Validators.required),
-          weight: new FormControl('', Validators.required),
-          amount: new FormControl('', Validators.required),
-          gender: new FormControl('', Validators.required),
-          lengthMeasurement: new FormControl('', Validators.required),
-          comment: new FormControl('', Validators.required)
-        }));
+  ngAfterViewChecked(): void {
+  }
 
-        commentField.removeEventListener('keydown', listener);
-        this.hasListener = false;
-      }
+  onKeyPress(event: KeyboardEvent) {
+    if (event.key === 'Tab') {
+      this.items = this.measurementsForm.get('items') as FormArray;
+      this.items.push(this.createItem());
     }
+  }
 
-    commentField.addEventListener('keydown', listener);
-    this.hasListener = true;
+  isInvalid(i) {
+    return (<FormArray>this.measurementsForm.get('items')).controls[i].invalid;
   }
 }
