24f6eff9d48d13d0926372e058eaf564648d0b15
MichielDx <michieldx@gmail.com>
2021-03-22 11:00:59 +0100

    Refactor

--- src/app/vis/survey-events/survey-event-measurements-create-page/survey-event-measurements-create-page.component.ts
+++ src/app/vis/survey-events/survey-event-measurements-create-page/survey-event-measurements-create-page.component.ts
@@ -1,15 +1,15 @@
-import {AfterViewChecked, AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavigationLink} from '../../../shared-ui/layouts/NavigationLinks';
 import {GlobalConstants} from '../../../GlobalConstants';
 import {BreadcrumbLink} from '../../../shared-ui/breadcrumb/BreadcrumbLinks';
 import {ActivatedRoute} from '@angular/router';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-survey-event-measurements-create-page',
   templateUrl: './survey-event-measurements-create-page.component.html'
 })
-export class SurveyEventMeasurementsCreatePageComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class SurveyEventMeasurementsCreatePageComponent implements OnInit {
 
   links: NavigationLink[] = GlobalConstants.links;
   breadcrumbLinks: BreadcrumbLink[] = [
@@ -36,17 +36,14 @@ export class SurveyEventMeasurementsCreatePageComponent implements OnInit, After
     }
   ];
 
-  private measurementsForm: FormGroup;
-  private items: FormArray;
+  measurementsForm: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) {
-
-
   }
 
   ngOnInit(): void {
     this.measurementsForm = this.formBuilder.group({
-      items: this.formBuilder.array([ this.createItem() ])
+      items: this.formBuilder.array([this.createItem()])
     });
   }
 
@@ -62,20 +59,14 @@ export class SurveyEventMeasurementsCreatePageComponent implements OnInit, After
     });
   }
 
-  ngAfterViewInit() {
-  }
-
-  ngAfterViewChecked(): void {
+  items(): FormArray {
+    return this.measurementsForm.get('items') as FormArray;
   }
 
   onKeyPress(event: KeyboardEvent, i: number) {
-    if (event.key === 'Tab' && (this.items === undefined || (i+1) == this.items.length)) {
-      this.items = this.measurementsForm.get('items') as FormArray;
-      this.items.push(this.createItem());
+    if (event.key === 'Tab' && (this.items() === undefined || (i + 1) === this.items().length)) {
+      console.log(this.items());
+      this.items().push(this.createItem());
     }
-  }
-
-  isInvalid(i) {
-    return (<FormArray>this.measurementsForm.get('items')).controls[i].invalid;
   }
 }
