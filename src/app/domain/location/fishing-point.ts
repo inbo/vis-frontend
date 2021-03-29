eb45c9850cdf0487f63830faf87f776f853467e5
kevin <kevin.vandenelshout@gmail.com>
2021-03-29 10:06:05 +0200

    vha entity mapping
    Issues: #80

--- src/app/domain/location/fishing-point.ts
+++ src/app/domain/location/fishing-point.ts
@@ -5,6 +5,25 @@ export interface FishingPoint {
   slope: number;
   x: number;
   y: number;
+
+  snappedX: number;
+  snappedY: number;
+  vhaId: number;
+  vhag: number;
+  name: string;
+  categoryValue: number;
+  categoryName: string;
+  basinNumber: number;
+  basinName: string;
+  flowArea: string;
+  qualityTarget: number;
+  qualityLabel: string;
+  qualityLabelGeo: string;
+  zoneNumber: number;
+  waterCode: string;
+  length: number;
+  puddleVersion: string;
+  puddleCode: string;
 }
 
 
