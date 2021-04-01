6051b636864d137bc5971684e6e491ddf760b994
kevin <kevin.vandenelshout@gmail.com>
2021-04-01 08:47:40 +0200

    use tailwindcss jit
    https://www.npmjs.com/package/ng-tailwindcss-jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,14 @@
 module.exports = {
-  prefix: '',
-  purge: {
-    enabled: true,
-    content: ['**/*.html', '**/*.ts']
-  },
+  purge: [
+    '**/*.html',
+    '**/*.ts'
+  ],
   theme: {
     extend: {
       boxShadow: {
         'under': '0 2px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
       }
     },
-  },
-  variants: {
-    extend: {
-      opacity: ['disabled'],
-    }
   },
   plugins: [
     require('@tailwindcss/typography'),
