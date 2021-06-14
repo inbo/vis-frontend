04ddc7617dc8c43f5e60889bbe99c5436caf6bb8
MichielDx <michieldx@gmail.com>
2021-06-14 10:33:59 +0200

    Fix tips breadcrumb
    #118

--- src/app/vis/tips/tips/tips.component.ts
+++ src/app/vis/tips/tips/tips.component.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {NavigationLink} from '../../../shared-ui/layouts/NavigationLinks';
 import {GlobalConstants} from '../../../GlobalConstants';
 import {BreadcrumbLink} from '../../../shared-ui/breadcrumb/BreadcrumbLinks';
-import {ActivatedRoute, Router} from '@angular/router';
-import {TranslateService} from '@ngx-translate/core';
-import {TipsService} from '../../../services/vis.tips.service';
 
 @Component({
   selector: 'app-tips',
@@ -14,15 +11,10 @@ export class TipsComponent implements OnInit {
 
   links: NavigationLink[] = GlobalConstants.links;
   breadcrumbLinks: BreadcrumbLink[] = [
-    {title: 'Tips', url: '/tips'},
-    {
-      title: this.translateService.instant(this.activatedRoute.snapshot.params.tipPage),
-      url: '/tips/' + this.activatedRoute.snapshot.params.tipPage
-    },
-    {title: '', url: ''}
+    {title: 'Tips', url: '/tips/COMMON'}
   ];
 
-  constructor(private activatedRoute: ActivatedRoute, private translateService: TranslateService) {
+  constructor() {
   }
 
   ngOnInit(): void {
