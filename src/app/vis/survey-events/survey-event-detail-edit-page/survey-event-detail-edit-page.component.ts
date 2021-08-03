99c6167c8ad439637afee99126f382b118e1526f
kevin <kevin.vandenelshout@gmail.com>
2021-08-03 07:32:07 +0200

    refactor searchable select
    - remove translations from the component
    - make the templating dynamic and provide a default

--- src/app/vis/survey-events/survey-event-detail-edit-page/survey-event-detail-edit-page.component.ts
+++ src/app/vis/survey-events/survey-event-detail-edit-page/survey-event-detail-edit-page.component.ts
@@ -1,16 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Title} from '@angular/platform-browser';
 import {ActivatedRoute, Router} from '@angular/router';
 import {SurveyEventsService} from '../../../services/vis.surveyevents.service';
-import {Option} from '../../../shared-ui/searchable-select/option';
-import {Subject} from 'rxjs';
+import {SearchableSelectOption} from '../../../shared-ui/searchable-select/option';
 import {map, take} from 'rxjs/operators';
 import {LocationsService} from '../../../services/vis.locations.service';
 import {Method} from '../../../domain/method/method';
 import {MethodsService} from '../../../services/vis.methods.service';
 import {Role} from '../../../core/_models/role';
 import {SurveyEvent} from '../../../domain/survey-event/surveyEvent';
+import {TranslateService} from '@ngx-translate/core';
+import {FishingPointSearch} from '../../../domain/location/fishing-point';
 
 @Component({
   selector: 'app-survey-event-detail-edit-page',
@@ -22,16 +22,14 @@ export class SurveyEventDetailEditPageComponent implements OnInit {
 
   surveyEventForm: FormGroup;
   submitted = false;
-  methods: Method[];
   surveyEvent: SurveyEvent;
 
-  locations$ = new Subject<Option[]>();
-  methods$ = new Subject<Option[]>();
-
+  locations: SearchableSelectOption[] = [];
+  methods: SearchableSelectOption[] = [];
 
   constructor(private surveyEventService: SurveyEventsService, private activatedRoute: ActivatedRoute,
               private router: Router, private formBuilder: FormBuilder, private locationsService: LocationsService,
-              private methodsService: MethodsService) {
+              private methodsService: MethodsService, private translateService: TranslateService) {
   }
 
   ngOnInit(): void {
@@ -50,45 +48,39 @@ export class SurveyEventDetailEditPageComponent implements OnInit {
         this.surveyEvent = surveyEvent;
 
         this.occurrenceDate.patchValue(new Date(surveyEvent.occurrence));
-        this.location.patchValue({
-          id: surveyEvent.fishingPoint?.id,
-          translateKey: `fishing-point.id.${surveyEvent.fishingPoint?.id}.code`,
-          secondaryTranslateKey: `fishing-point.id.${surveyEvent.fishingPoint?.id}`
-        });
-        this.surveyEventForm.get('method').patchValue({id: surveyEvent.method, translateKey: `method.${surveyEvent.method}`});
+        this.location.patchValue(surveyEvent.fishingPoint?.id);
         this.comment.patchValue(surveyEvent.comment);
+
+        this.getLocations(null);
       });
 
-    this.methodsService.getAllMethods().pipe(take(1))
-      .subscribe(methods => {
-        this.methods$.next(methods.map(this.mapMethodToOption()));
-        return this.methods = methods;
-      });
+    this.getMethods(null);
   }
 
   getLocations(val: any) {
-    this.locationsService.searchFishingPoints(val).pipe(
+    this.locationsService.searchFishingPoints(val, this.surveyEvent?.fishingPoint?.id).pipe(
+      take(1),
       map(fishingPoints => {
         return fishingPoints.map(fishingPoint => ({
-          id: fishingPoint.id,
-          translateKey: `fishing-point.id.${fishingPoint.id}.code`,
-          secondaryTranslateKey: `fishing-point.id.${fishingPoint.id}`
+          selectValue: fishingPoint.id,
+          option: fishingPoint
         }));
       })
-    ).subscribe(value => this.locations$.next(value));
+    ).subscribe(value => this.locations = value as any as SearchableSelectOption[]);
   }
 
   getMethods(val: string) {
-    this.methods$.next(
-      this.methods.filter(value => value.description.toLowerCase().includes(val))
-        .map(this.mapMethodToOption())
-    );
-  }
+    this.methodsService.getAllMethods().pipe(
+      take(1),
+      map((values: Method[]) => val === null ? values : values.filter(value => value.description.toLowerCase().includes(val))),
+      map(methods => {
+        return methods.map(method => ({
+          selectValue: method.code,
+          option: method
+        }));
+      })
+    ).subscribe(value => this.methods = value as any as SearchableSelectOption[]);
 
-  mapMethodToOption() {
-    return value => {
-      return {id: value.code, translateKey: `method.${value.code}`};
-    };
   }
 
   saveSurveyEvent() {
@@ -99,8 +91,7 @@ export class SurveyEventDetailEditPageComponent implements OnInit {
     }
 
     const formData = this.surveyEventForm.getRawValue();
-    formData.fishingPointId = formData.location.id;
-    formData.method = formData.method.id;
+    formData.fishingPointId = formData.location;
     delete formData.location;
 
     this.surveyEventService.updateSurveyEvent(this.activatedRoute.parent.snapshot.params.projectCode,
@@ -134,6 +125,10 @@ export class SurveyEventDetailEditPageComponent implements OnInit {
 
   get location() {
     return this.surveyEventForm.get('location');
+  }
+
+  get method() {
+    return this.surveyEventForm.get('method');
   }
 
   get comment() {
