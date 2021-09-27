26895ad52df8a09ef3cabd7b14f949b216810155
kevin <kevin.vandenelshout@gmail.com>
2021-09-27 13:39:15 +0200

    refactor tables to table component/directives

--- src/app/shared-ui/table/directives/cell.directive.ts
+++ src/app/shared-ui/table/directives/cell.directive.ts
@@ -8,14 +8,17 @@ export class CellDirective implements OnInit {
   @Input()
   actions = false;
 
+  @Input()
+  condense = false;
+
   constructor(private el: ElementRef) {
   }
 
   ngOnInit(): void {
-    (this.el.nativeElement as HTMLElement).classList.add('px-6', 'py-4', 'text-sm', 'text-gray-500');
+    (this.el.nativeElement as HTMLElement).classList.add('px-6', this.condense ? 'py-1' : 'py-4', 'text-sm', 'text-gray-500');
 
     if (this.actions) {
-      (this.el.nativeElement as HTMLElement).classList.add('text-right');
+      (this.el.nativeElement as HTMLElement).classList.add('text-right', 'font-medium');
     }
   }
 
