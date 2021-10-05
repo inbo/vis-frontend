d24957f4eda0770b1955279528cb8cecf92968dd
kevin <kevin.vandenelshout@gmail.com>
2021-10-05 12:46:52 +0200

    empty state indien er geen afbeeldingen zijn
    issues: #23

--- src/app/vis/project/project-pictures-page/project-pictures-page.component.ts
+++ src/app/vis/project/project-pictures-page/project-pictures-page.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Project} from '../../../domain/project/project';
 import {Title} from '@angular/platform-browser';
 import {ActivatedRoute} from '@angular/router';
 import {TandemvaultPicture, TandemvaultPictureDetail} from '../../../domain/tandemvault/picture';
 import {PicturesService} from '../../../services/vis.pictures.service';
 import {AsyncPage} from '../../../shared-ui/paging-async/asyncPage';
 import {Subscription} from 'rxjs';
+import {ProjectService} from '../../../services/vis.project.service';
+import {Project} from '../../../domain/project/project';
 
 @Component({
   selector: 'app-project-pictures-page',
@@ -17,14 +18,21 @@ export class ProjectPicturesPageComponent implements OnInit, OnDestroy {
   pager: AsyncPage<TandemvaultPicture>;
   pictures: TandemvaultPicture[];
 
-  project: Project;
   detail: TandemvaultPictureDetail;
   selectedPicture: TandemvaultPicture;
 
   subscription = new Subscription();
+  projectCode: string;
+  tandemvaultcollectionslug: string;
 
-  constructor(private titleService: Title, private activatedRoute: ActivatedRoute, private picturesService: PicturesService) {
-    this.titleService.setTitle(`Project ${this.activatedRoute.snapshot.parent.params.projectCode} afbeeldingen`);
+  constructor(private titleService: Title, private activatedRoute: ActivatedRoute,
+              private picturesService: PicturesService, private projectService: ProjectService) {
+    this.projectCode = this.activatedRoute.snapshot.parent.params.projectCode;
+    this.titleService.setTitle(`Project ${this.projectCode} afbeeldingen`);
+
+    this.projectService.getProject(this.projectCode).subscribe(value => {
+      this.tandemvaultcollectionslug = value.tandemvaultcollectionslug;
+    });
 
     this.loadPicturesPage();
 
@@ -61,7 +69,7 @@ export class ProjectPicturesPageComponent implements OnInit, OnDestroy {
     this.pictures = [];
     const page = this.activatedRoute.snapshot.queryParams.page;
     this.subscription.add(
-      this.picturesService.getPictures(page, this.activatedRoute.snapshot.parent.params.projectCode).subscribe((value) => {
+      this.picturesService.getPictures(page, this.projectCode).subscribe((value) => {
         this.pager = value;
         this.pictures = value.content;
         this.loading = false;
