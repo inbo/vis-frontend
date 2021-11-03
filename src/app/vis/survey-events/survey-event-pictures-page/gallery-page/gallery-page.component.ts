59bc05612abe86eb7b95f28dce1d8d53d7aa067e
kevin <kevin.vandenelshout@gmail.com>
2021-11-03 11:31:29 +0100

    pictures updated routes
    Issues: #179

---
+++ src/app/vis/survey-events/survey-event-pictures-page/gallery-page/gallery-page.component.ts
@@ -0,0 +1,116 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AsyncPage} from '../../../../shared-ui/paging-async/asyncPage';
+import {TandemvaultPicture, TandemvaultPictureDetail} from '../../../../domain/tandemvault/picture';
+import {Subscription} from 'rxjs';
+import {Title} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+import {PicturesService} from '../../../../services/vis.pictures.service';
+import {ProjectService} from '../../../../services/vis.project.service';
+
+@Component({
+  selector: 'app-gallery-page',
+  templateUrl: './gallery-page.component.html'
+})
+export class GalleryPageComponent implements OnInit, OnDestroy {
+  loading = false;
+
+  pager: AsyncPage<TandemvaultPicture>;
+  pictures: TandemvaultPicture[];
+
+  detail: TandemvaultPictureDetail;
+  selectedPicture: TandemvaultPicture;
+
+  subscription = new Subscription();
+  projectCode: string;
+  surveyEventId: number;
+  url: string;
+  tandemvaultcollectionslug: string;
+
+  constructor(private titleService: Title, private activatedRoute: ActivatedRoute,
+              private picturesService: PicturesService, private projectService: ProjectService) {
+    this.projectCode = this.activatedRoute.snapshot.parent.parent.params.projectCode;
+    this.surveyEventId = this.activatedRoute.snapshot.parent.parent.params.surveyEventId;
+    this.url = this.activatedRoute.snapshot.data.url;
+    this.titleService.setTitle(`Waarneming afbeeldingen`);
+
+    this.projectService.getProject(this.projectCode).subscribe(value => {
+      this.tandemvaultcollectionslug = value.tandemvaultcollectionslug;
+    });
+
+    this.loadPicturesPage();
+
+    this.subscription.add(
+      this.activatedRoute.queryParams.subscribe((params) => {
+          this.loadPicturesPage();
+        }
+      ));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  openDetail(picture: TandemvaultPicture) {
+    this.selectedPicture = picture;
+    this.loadDetailPicture(picture.id);
+  }
+
+  private loadDetailPicture(id: number) {
+    this.picturesService.getPicture(id).subscribe(value => {
+      this.detail = value;
+    });
+  }
+
+  download(id: any) {
+    this.picturesService.downloadPicture(id).subscribe(value => {
+      window.open(value.url, '_blank');
+    });
+  }
+
+  loadPicturesPage() {
+    this.loading = true;
+    this.pictures = [];
+    const page = this.activatedRoute.snapshot.queryParams.page;
+
+    if (this.url === 'afbeeldingen') {
+      this.subscription.add(
+        this.picturesService.getPicturesForSurveyEvent(page, this.projectCode, this.surveyEventId).subscribe((value) => {
+          this.afterLoadPictures(value);
+        })
+      );
+    } else if (this.url === 'project') {
+      this.subscription.add(
+        this.picturesService.getPictures(page, this.projectCode).subscribe((value) => {
+          this.afterLoadPictures(value);
+        })
+      );
+    } else if (this.url === 'dag') {
+      this.subscription.add(
+        this.picturesService.getPicturesForSurveyEvent(page, this.projectCode, this.surveyEventId).subscribe((value) => {
+          this.afterLoadPictures(value);
+        })
+      );
+    }
+
+  }
+
+  private afterLoadPictures(value: AsyncPage<TandemvaultPicture>) {
+    this.pager = value;
+    this.pictures = value.content;
+    this.loading = false;
+    this.selectedPicture = this.pictures[0];
+    this.openDetail(this.selectedPicture);
+  }
+
+  addTags(assetId: number) {
+    this.subscription.add(
+      this.picturesService.addTagsForSurveyEvent(assetId, this.projectCode, this.surveyEventId).subscribe(value => {
+        this.loadDetailPicture(assetId);
+      })
+    );
+  }
+}
---
+++ src/app/vis/survey-events/survey-event-pictures-page/upload-page/upload-page.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-upload-page',
+  templateUrl: './upload-page.component.html'
+})
+export class UploadPageComponent implements OnInit {
+  projectCode: string;
+  surveyEventId: number;
+
+  constructor(private titleService: Title, private activatedRoute: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.projectCode = this.activatedRoute.snapshot.parent.parent.params.projectCode;
+    this.surveyEventId = this.activatedRoute.snapshot.parent.parent.params.surveyEventId;
+  }
+
+}
