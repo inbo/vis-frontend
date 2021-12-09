1657a476b40a795b06b063204330bf88c346d7fd
kevin <kevin.vandenelshout@gmail.com>
2021-12-09 10:18:45 +0100

    couple pictures to subcollections
    Issues: #262

--- src/app/vis/survey-events/survey-event-pictures-page/survey-event-pictures-page.component.ts
+++ src/app/vis/survey-events/survey-event-pictures-page/survey-event-pictures-page.component.ts
@@ -4,40 +4,43 @@ import {Title} from '@angular/platform-browser';
 import {ActivatedRoute} from '@angular/router';
 import {PicturesService} from '../../../services/vis.pictures.service';
 import {ProjectService} from '../../../services/vis.project.service';
+import {environment} from '../../../../environments/environment';
 
 @Component({
-    selector: 'app-survey-event-pictures-page',
-    templateUrl: './survey-event-pictures-page.component.html'
+  selector: 'app-survey-event-pictures-page',
+  templateUrl: './survey-event-pictures-page.component.html'
 })
 export class SurveyEventPicturesPageComponent implements OnInit, OnDestroy {
-    loading = true;
+  loading = true;
 
-    subscription = new Subscription();
-    projectCode: string;
-    surveyEventId: number;
-    url: string;
-    tandemvaultcollectionslug: string;
+  subscription = new Subscription();
+  projectCode: string;
+  surveyEventId: number;
+  url: string;
+  tandemvaultcollectionslug: string;
+  showWarning = false;
 
-    constructor(private titleService: Title, private activatedRoute: ActivatedRoute,
-                private picturesService: PicturesService, private projectService: ProjectService) {
-        this.projectCode = this.activatedRoute.snapshot.parent.params.projectCode;
-        this.surveyEventId = this.activatedRoute.snapshot.parent.params.surveyEventId;
-        this.url = this.activatedRoute.snapshot.data.url;
-        this.titleService.setTitle(`Waarneming afbeeldingen`);
+  constructor(private titleService: Title, private activatedRoute: ActivatedRoute,
+              private picturesService: PicturesService, private projectService: ProjectService) {
+    this.projectCode = this.activatedRoute.snapshot.parent.params.projectCode;
+    this.surveyEventId = this.activatedRoute.snapshot.parent.params.surveyEventId;
+    this.url = this.activatedRoute.snapshot.data.url;
+    this.titleService.setTitle(`Waarneming afbeeldingen`);
 
-        this.projectService.getProject(this.projectCode).subscribe(value => {
-            this.tandemvaultcollectionslug = value.tandemvaultcollectionslug;
-            this.loading = false;
-        });
+    this.projectService.getProject(this.projectCode).subscribe(value => {
+      this.tandemvaultcollectionslug = value.tandemvaultcollectionslug;
+      this.loading = false;
+    });
 
-    }
+    this.showWarning = environment.env === 'local' || environment.env === 'dev' || environment.env === 'uat';
+  }
 
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
-    }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
 
-    }
+  }
 
 }
