ef46c73ef71190f044dd76d2dfb1c691a080a867
kevin <kevin.vandenelshout@gmail.com>
2021-12-20 09:29:05 +0100

    update prd config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://vis.inbo.be',
   keycloakClientId: 'vis-prd',
-  keycloakIssuer: "https://keycloak-prd.inbo.be/auth/realms/Inbo-Extranet",
+  keycloakIssuer: 'https://keycloak.inbo.be/auth/realms/Inbo-Extranet',
   showDebugInformation: false
 };
