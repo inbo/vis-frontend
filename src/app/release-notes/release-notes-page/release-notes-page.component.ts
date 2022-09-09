404d8241bc93a8ca8a60e394a8f03dd1ae343bc5
ivar <ivar.vanhartingsveldt@inbo.be>
2022-09-09 10:26:06 +0200

    - Added new release notes for v2.1

--- src/app/release-notes/release-notes-page/release-notes-page.component.ts
+++ src/app/release-notes/release-notes-page/release-notes-page.component.ts
@@ -7,54 +7,56 @@ import {Subscription} from 'rxjs';
 import {ReleaseNotesService} from '../../services/vis.release-notes.service';
 
 @Component({
-  selector: 'app-release-notes-page',
-  templateUrl: './release-notes-page.component.html'
+    selector: 'app-release-notes-page',
+    templateUrl: './release-notes-page.component.html',
 })
 export class ReleaseNotesPageComponent implements OnInit, OnDestroy {
-  links: NavigationLink[] = GlobalConstants.links;
-  breadcrumbLinks: BreadcrumbLink[] = [
-    {title: 'Release notes', url: '/releases'}
-  ];
-  releaseAsHtml: string;
-  releases: string[];
-  private currentRelease: string;
-  private subscription = new Subscription();
+    links: Array<NavigationLink> = GlobalConstants.links;
+    breadcrumbLinks: Array<BreadcrumbLink> = [
+        {title: 'Release notes', url: '/releases'},
+    ];
+    releaseAsHtml: string;
+    releases: Array<string>;
+    private currentRelease: string;
+    private subscription = new Subscription();
 
-  constructor(private releaseNotesService: ReleaseNotesService, private activatedRoute: ActivatedRoute, private router: Router) {
-    this.subscription.add(
-      this.activatedRoute.params.subscribe(res => {
-        if (!res.release || res.release !== this.currentRelease) {
-          this.loadReleases();
-          this.currentRelease = res.release;
-        }
-      })
-    );
-  }
-
-  ngOnInit(): void {
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-
-  loadReleases() {
-    const release = this.activatedRoute.snapshot.params.release;
-    if (!release) {
-      this.subscription.add(
-        this.releaseNotesService.getLatestRelease().subscribe(value => {
-          this.router.navigate(['/releases/' + value]);
-        })
-      );
-    } else {
-      this.subscription.add(
-        this.releaseNotesService.getReleases(release).subscribe(value => {
-          this.releaseAsHtml = value.releaseAsHtml;
-          this.releases = value.releaseVersions.reverse();
-        })
-      );
+    constructor(private releaseNotesService: ReleaseNotesService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) {
+        this.subscription.add(
+            this.activatedRoute.params.subscribe(res => {
+                if (!res.release || res.release !== this.currentRelease) {
+                    this.loadReleases();
+                    this.currentRelease = res.release;
+                }
+            }),
+        );
     }
-  }
+
+    ngOnInit(): void {
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+
+    loadReleases() {
+        const release = this.activatedRoute.snapshot.params.release;
+        if (!release) {
+            this.subscription.add(
+                this.releaseNotesService.getLatestRelease().subscribe(value => {
+                    this.router.navigate(['/releases/' + value]);
+                }),
+            );
+        } else {
+            this.subscription.add(
+                this.releaseNotesService.getReleases(release).subscribe(value => {
+                    this.releaseAsHtml = value.releaseAsHtml;
+                    this.releases = value.releaseVersions.reverse();
+                }),
+            );
+        }
+    }
 
 }
