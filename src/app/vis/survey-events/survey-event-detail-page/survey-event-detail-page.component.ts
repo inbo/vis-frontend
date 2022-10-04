e6cfc18056b0a79960bcb8d2c5eee8fe65d80374
ivar <ivar.vanhartingsveldt@inbo.be>
2022-10-04 09:24:59 +0200

    - Listen for changes in the route params to load the correct survey events in survey event detail page - Removed a redundant check in the backend - Fixed the check in the backend to check the correct fishing point

--- src/app/vis/survey-events/survey-event-detail-page/survey-event-detail-page.component.ts
+++ src/app/vis/survey-events/survey-event-detail-page/survey-event-detail-page.component.ts
@@ -1,77 +1,88 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Title} from '@angular/platform-browser';
 import {ActivatedRoute} from '@angular/router';
 import {SurveyEventsService} from '../../../services/vis.surveyevents.service';
 import {SurveyEvent} from '../../../domain/survey-event/surveyEvent';
 import {Role} from '../../../core/_models/role';
-import {take} from 'rxjs/operators';
+import {switchMap, take, takeUntil, tap} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
-  selector: 'app-survey-event-detail-page',
-  templateUrl: './survey-event-detail-page.component.html'
+    selector: 'app-survey-event-detail-page',
+    templateUrl: './survey-event-detail-page.component.html',
 })
 
-export class SurveyEventDetailPageComponent implements OnInit {
-  role = Role;
-  surveyEvent: SurveyEvent;
-  isModalOpen = false;
-  projectCode: string;
+export class SurveyEventDetailPageComponent implements OnInit, OnDestroy {
 
-  constructor(private titleService: Title, private activatedRoute: ActivatedRoute, private surveyEventService: SurveyEventsService) {
-    this.titleService.setTitle(`Waarneming algemeen ${this.activatedRoute.parent.snapshot.params.surveyEventId}`);
-    this.loadSurveyEvent();
-  }
+    role = Role;
+    surveyEvent: SurveyEvent;
+    isModalOpen = false;
+    projectCode: string;
+    private readonly destroy = new Subject<void>();
 
-  private loadSurveyEvent() {
-    this.projectCode = this.activatedRoute.parent.snapshot.params.projectCode;
-    this.surveyEventService.getSurveyEvent(this.projectCode,
-      this.activatedRoute.parent.snapshot.params.surveyEventId).subscribe(value => this.surveyEvent = value);
-  }
+    constructor(private titleService: Title, private activatedRoute: ActivatedRoute, private surveyEventService: SurveyEventsService) {
+    }
 
-  ngOnInit(): void {
-  }
-
-  reopenSurveyEvent() {
-    this.surveyEventService.reOpenSurveyEvent(this.activatedRoute.snapshot.params.projectCode,
-      this.activatedRoute.parent.snapshot.params.surveyEventId)
-      .pipe(take(1))
-      .subscribe(() => location.reload());
-  }
-
-  validateSurveyEvent() {
-    this.surveyEventService.validateSurveyEvent(this.activatedRoute.snapshot.params.projectCode,
-      this.activatedRoute.parent.snapshot.params.surveyEventId)
-      .pipe(take(1))
-      .subscribe(() => location.reload());
-  }
-
-  deleteSurveyEvent() {
-    this.isModalOpen = true;
-  }
-
-  canValidateSurveyEvent() {
-    return this.surveyEvent?.status === 'ENTERED';
-  }
-
-  canRestoreSurveyEvent() {
-    return this.surveyEvent?.status === 'DELETED' || this.surveyEvent?.status === 'VALID';
-  }
-
-  canDeleteSurveyEvent() {
-    return this.surveyEvent?.status === 'ENTERED';
-  }
-
-  cancelModal() {
-    this.isModalOpen = false;
-  }
-
-  confirmClicked() {
-    this.surveyEventService.deleteSurveyEvent(this.activatedRoute.parent.snapshot.params.projectCode,
-      this.activatedRoute.parent.snapshot.params.surveyEventId)
-      .pipe(take(1))
-      .subscribe(() => {
-        this.cancelModal();
+    ngOnInit(): void {
+        this.titleService.setTitle(`Waarneming algemeen ${this.activatedRoute.parent.snapshot.params.surveyEventId}`);
         this.loadSurveyEvent();
-      });
-  }
+    }
+
+    ngOnDestroy() {
+        this.destroy.next(undefined);
+    }
+
+    reopenSurveyEvent() {
+        this.surveyEventService.reOpenSurveyEvent(this.activatedRoute.snapshot.params.projectCode,
+            this.activatedRoute.parent.snapshot.params.surveyEventId)
+            .pipe(take(1))
+            .subscribe(() => location.reload());
+    }
+
+    validateSurveyEvent() {
+        this.surveyEventService.validateSurveyEvent(this.activatedRoute.snapshot.params.projectCode,
+            this.activatedRoute.parent.snapshot.params.surveyEventId)
+            .pipe(take(1))
+            .subscribe(() => location.reload());
+    }
+
+    deleteSurveyEvent() {
+        this.isModalOpen = true;
+    }
+
+    canValidateSurveyEvent() {
+        return this.surveyEvent?.status === 'ENTERED';
+    }
+
+    canRestoreSurveyEvent() {
+        return this.surveyEvent?.status === 'DELETED' || this.surveyEvent?.status === 'VALID';
+    }
+
+    canDeleteSurveyEvent() {
+        return this.surveyEvent?.status === 'ENTERED';
+    }
+
+    cancelModal() {
+        this.isModalOpen = false;
+    }
+
+    confirmClicked() {
+        this.surveyEventService.deleteSurveyEvent(this.activatedRoute.parent.snapshot.params.projectCode,
+            this.activatedRoute.parent.snapshot.params.surveyEventId)
+            .pipe(take(1))
+            .subscribe(() => {
+                this.cancelModal();
+                this.loadSurveyEvent();
+            });
+    }
+
+    private loadSurveyEvent() {
+        this.activatedRoute.params
+            .pipe(
+                takeUntil(this.destroy.asObservable()),
+                tap(params => this.projectCode = params.projectCode),
+                switchMap(params => this.surveyEventService.getSurveyEvent(this.projectCode, params.surveyEventId)),
+            )
+            .subscribe(value => this.surveyEvent = value);
+    }
 }
