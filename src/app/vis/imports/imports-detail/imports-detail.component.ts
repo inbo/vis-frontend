85f4eb8e6816c9be49fd2048381ef901bf4f9859
Madi <madi.dudaeva@inbo.be>
2023-05-25 08:16:49 +0200

    VIS-518: Add some improvements for the validation of measurements. Show success message after import. Fix typo in a package name.

--- src/app/vis/imports/imports-detail/imports-detail.component.ts
+++ src/app/vis/imports/imports-detail/imports-detail.component.ts
@@ -8,6 +8,9 @@ import {ImportsService} from '../../../services/vis.imports.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ImportDetail} from '../../../domain/imports/imports';
 import {Role} from '../../../core/_models/role';
+import {HttpErrorResponse} from '@angular/common/http';
+import {ToastrService} from 'ngx-toastr';
+import {AlertService} from '../../../_alert';
 
 @Component({
   selector: 'vis-imports-detail',
@@ -34,14 +37,19 @@ export class ImportsDetailComponent implements OnInit, OnDestroy {
   hasInvalidDocument = true;
 
   constructor(private titleService: Title, private importsService: ImportsService, private activatedRoute: ActivatedRoute,
-              private router: Router) {
+              private router: Router, private toastr: ToastrService,
+              private alertService: AlertService) {
     this.titleService.setTitle('Imports');
     this.id = this.activatedRoute.snapshot.params.id;
     this.importsService.getImport(this.id).subscribe(value => {
       this.importDetail = value;
       this.loading = false;
       this.setIsDocumentValid();
-    });
+    },
+        error => {
+        alertService.error('Validatie fouten', 'Het bewaren is niet gelukt, controleer alle gegevens of contacteer een verantwoordelijke.')
+        }
+    );
   }
 
   ngOnInit(): void {
@@ -52,16 +60,19 @@ export class ImportsDetailComponent implements OnInit, OnDestroy {
   }
 
   doImport() {
-    this.importsService.doImport(this.id).subscribe(value => {
-      console.log(value); // TODO remove? backend returns void
+    this.importsService.doImport(this.id).subscribe({
+        next: () => {
+            this.alertService.success('Het importeren is gelukt', "De gegevens zijn opgeslagen", false);
+        }
     });
   }
 
-  private setIsDocumentValid(): void {
+    private setIsDocumentValid(): void {
       // TODO: this should preferably be set by the backend.
       this.hasInvalidDocument = !this.importDetail.items || this.importDetail.items.filter(item => {
           const hasInvalidSurveyEvent = item.surveyEvents.filter(surveyEvent => {
-              return !(surveyEvent.fishingPoint.valid && surveyEvent.method.valid && surveyEvent.occurrence.valid && !surveyEvent.existingSurveyEventId);
+              const notValidMeasurements = surveyEvent.measurements.filter(measurement => {return !measurement.valid}).length > 0;
+              return !(surveyEvent.fishingPoint.valid && surveyEvent.method.valid && surveyEvent.occurrence.valid && !surveyEvent.existingSurveyEventId) || notValidMeasurements;
           }).length > 0;
           return !item.project.valid || hasInvalidSurveyEvent;
       }).length > 0;
