4ac2e610e99019dffaf6090d49f7be014fdda58e
JurgenDK <jurgen.deketelaere@inbo.be>
2025-05-22 13:19:35 +0200

    Setup github actions and version file

---
+++ src/app/services/version.ts
@@ -0,0 +1,8 @@
+// This file is used to hold the application's version information
+// placeholders need to be replaced with actual values at build time (see https://github.com/inbo/inbo-github-actions)
+export const appInfo = {
+  application: 'vis-frontend',
+  version: '$VERSION',
+  gitCommitId: '$GIT_COMMIT',
+  buildTime: '$BUILD_TIME',
+};
